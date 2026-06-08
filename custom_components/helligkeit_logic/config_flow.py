8a5733c204a66e94b902eef6f4be5ada3cb9d1a4
Pascal Höfer <pascal.hoefer@gmail.com>
2026-06-08 18:55:51 +0200

    Fix config flow selectors for older Home Assistant versions in helligkeit_logic

--- custom_components/helligkeit_logic/config_flow.py
+++ custom_components/helligkeit_logic/config_flow.py
@@ -1,7 +1,33 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.helpers import selector
-from .const import *
+from homeassistant.helpers import config_validation as cv
+
+try:
+    from homeassistant.helpers import selector
+except ImportError:  # pragma: no cover
+    selector = None
+
+from .const import (
+    CONF_ELEVATION,
+    CONF_NAME,
+    CONF_SENSOR1,
+    CONF_SENSOR2,
+    CONF_SONNE1,
+    CONF_SONNE2,
+    CONF_TIME_SONNE,
+    CONF_TIME_WOLKE,
+    CONF_WOLKE1,
+    CONF_WOLKE2,
+    DEFAULT_NAME,
+    DOMAIN,
+)
+
+
+def _sensor_selector():
+    if selector is not None and hasattr(selector, "EntitySelector"):
+        return selector.EntitySelector({"domain": "sensor"})
+    return cv.entity_id
+
 
 class HelligkeitConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -12,9 +38,9 @@ class HelligkeitConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         schema = vol.Schema({
             vol.Required(CONF_NAME, default=DEFAULT_NAME): str,
-            vol.Required(CONF_SENSOR1): selector.EntitySelector({"domain": "sensor"}),
-            vol.Required(CONF_SENSOR2): selector.EntitySelector({"domain": "sensor"}),
-            vol.Required(CONF_ELEVATION): selector.EntitySelector({"domain": "sensor"}),
+            vol.Required(CONF_SENSOR1): _sensor_selector(),
+            vol.Required(CONF_SENSOR2): _sensor_selector(),
+            vol.Required(CONF_ELEVATION): _sensor_selector(),
             vol.Required(CONF_SONNE1, default=8000): int,
             vol.Required(CONF_WOLKE1, default=3000): int,
             vol.Required(CONF_SONNE2, default=8000): int,
@@ -42,9 +68,9 @@ class HelligkeitOptionsFlow(config_entries.OptionsFlow):
 
         schema = vol.Schema({
             vol.Required(CONF_NAME, default=current.get(CONF_NAME, DEFAULT_NAME)): str,
-            vol.Required(CONF_SENSOR1, default=current.get(CONF_SENSOR1)): selector.EntitySelector({"domain": "sensor"}),
-            vol.Required(CONF_SENSOR2, default=current.get(CONF_SENSOR2)): selector.EntitySelector({"domain": "sensor"}),
-            vol.Required(CONF_ELEVATION, default=current.get(CONF_ELEVATION)): selector.EntitySelector({"domain": "sensor"}),
+            vol.Required(CONF_SENSOR1, default=current.get(CONF_SENSOR1)): _sensor_selector(),
+            vol.Required(CONF_SENSOR2, default=current.get(CONF_SENSOR2)): _sensor_selector(),
+            vol.Required(CONF_ELEVATION, default=current.get(CONF_ELEVATION)): _sensor_selector(),
             vol.Required(CONF_SONNE1, default=current.get(CONF_SONNE1, 8000)): int,
             vol.Required(CONF_WOLKE1, default=current.get(CONF_WOLKE1, 3000)): int,
             vol.Required(CONF_SONNE2, default=current.get(CONF_SONNE2, 8000)): int,
