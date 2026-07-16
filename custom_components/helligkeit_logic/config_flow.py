0efca62a187cf8ea843be35889dc3acdf8d86fb7
Pascal Höfer <pascal.hoefer@gmail.com>
2026-07-16 13:59:26 +0200

    Fix config flow reconfiguration for existing entries
    add reconfigure step for existing integrations
    preserve and update current settings in the config flow
    improve labels for reconfigure/options dialog

--- custom_components/helligkeit_logic/config_flow.py
+++ custom_components/helligkeit_logic/config_flow.py
@@ -32,24 +32,39 @@ def _sensor_selector():
 class HelligkeitConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
+    def _build_schema(self, config_entry=None):
+        current = {}
+        if config_entry is not None:
+            current = {**config_entry.data, **config_entry.options}
+
+        return vol.Schema({
+            vol.Required(CONF_NAME, default=current.get(CONF_NAME, DEFAULT_NAME)): str,
+            vol.Required(CONF_SENSOR1, default=current.get(CONF_SENSOR1)): _sensor_selector(),
+            vol.Required(CONF_SENSOR2, default=current.get(CONF_SENSOR2)): _sensor_selector(),
+            vol.Required(CONF_ELEVATION, default=current.get(CONF_ELEVATION)): _sensor_selector(),
+            vol.Required(CONF_SONNE1, default=current.get(CONF_SONNE1, 8000)): int,
+            vol.Required(CONF_WOLKE1, default=current.get(CONF_WOLKE1, 3000)): int,
+            vol.Required(CONF_SONNE2, default=current.get(CONF_SONNE2, 8000)): int,
+            vol.Required(CONF_WOLKE2, default=current.get(CONF_WOLKE2, 3000)): int,
+            vol.Required(CONF_TIME_SONNE, default=current.get(CONF_TIME_SONNE, 3)): int,
+            vol.Required(CONF_TIME_WOLKE, default=current.get(CONF_TIME_WOLKE, 40)): int,
+        })
+
     async def async_step_user(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title=user_input.get(CONF_NAME, DEFAULT_NAME), data=user_input)
 
-        schema = vol.Schema({
-            vol.Required(CONF_NAME, default=DEFAULT_NAME): str,
-            vol.Required(CONF_SENSOR1): _sensor_selector(),
-            vol.Required(CONF_SENSOR2): _sensor_selector(),
-            vol.Required(CONF_ELEVATION): _sensor_selector(),
-            vol.Required(CONF_SONNE1, default=8000): int,
-            vol.Required(CONF_WOLKE1, default=3000): int,
-            vol.Required(CONF_SONNE2, default=8000): int,
-            vol.Required(CONF_WOLKE2, default=3000): int,
-            vol.Required(CONF_TIME_SONNE, default=3): int,
-            vol.Required(CONF_TIME_WOLKE, default=40): int,
-        })
+        return self.async_show_form(step_id="user", data_schema=self._build_schema())
 
-        return self.async_show_form(step_id="user", data_schema=schema)
+    async def async_step_reconfigure(self, user_input=None):
+        entry = self._get_reconfigure_entry()
+
+        if user_input is not None:
+            self.hass.config_entries.async_update_entry(entry, data=user_input)
+            await self.hass.config_entries.async_reload(entry.entry_id)
+            return self.async_abort(reason="reconfigure_successful")
+
+        return self.async_show_form(step_id="reconfigure", data_schema=self._build_schema(entry))
 
     async def async_get_options_flow(self, config_entry):
         return HelligkeitOptionsFlow(config_entry)
@@ -66,7 +81,7 @@ class HelligkeitOptionsFlow(config_entries.OptionsFlow):
             self.config_entry.async_update_entry(title=user_input.get(CONF_NAME, DEFAULT_NAME))
             return self.async_create_entry(title="", data=user_input)
 
-        schema = vol.Schema({
+        return self.async_show_form(step_id="init", data_schema=vol.Schema({
             vol.Required(CONF_NAME, default=current.get(CONF_NAME, DEFAULT_NAME)): str,
             vol.Required(CONF_SENSOR1, default=current.get(CONF_SENSOR1)): _sensor_selector(),
             vol.Required(CONF_SENSOR2, default=current.get(CONF_SENSOR2)): _sensor_selector(),
@@ -77,6 +92,4 @@ class HelligkeitOptionsFlow(config_entries.OptionsFlow):
             vol.Required(CONF_WOLKE2, default=current.get(CONF_WOLKE2, 3000)): int,
             vol.Required(CONF_TIME_SONNE, default=current.get(CONF_TIME_SONNE, 3)): int,
             vol.Required(CONF_TIME_WOLKE, default=current.get(CONF_TIME_WOLKE, 40)): int,
-        })
-
-        return self.async_show_form(step_id="init", data_schema=schema)
+        }))
